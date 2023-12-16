2b1c6bd48312eaed0b9a02136d18706c8ed9cb5c
holyjack <holyjackdev@gmail.com>
2023-12-16 19:43:29 +0400

    new origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socket_io_1 = require("socket.io");
 const httpServer = (0, http_1.createServer)();
 const io = new socket_io_1.Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://task6-front-two.vercel.app",
     },
 });
 const PORT = process.env.PORT || 80;
--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://task6-front-two.vercel.app",
   },
 });
 const PORT = process.env.PORT || 80;
